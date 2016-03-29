bf9ef748f9c77de7edbdc90488ba68b7b83ed073
Aleksii <sathen23@gmail.com>
2016-03-29 22:03:39 +0300

    Change Application.java add annotation config using java class

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,9 @@
+import com.aleksii.AppConfig;
 import com.aleksii.model.Customer;
 import com.aleksii.service.CustomerService;
 import com.aleksii.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Created by Sathen on 29.03.2016.
@@ -8,7 +11,9 @@ import com.aleksii.service.CustomerServiceImpl;
 public class Application {
     public static void main(String []args){
 
-         CustomerService service = new CustomerServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+         CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
 
         System.out.println(service.findAll().get(0).getFirstname());
 
