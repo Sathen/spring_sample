ba9f64ede551705faea2a32b4f4718ee0567713c
Aleksii <sathen23@gmail.com>
2016-03-29 18:00:59 +0300

    Create Setter Injection

--- src/com/aleksii/service/CustomerServiceImpl.java
+++ src/com/aleksii/service/CustomerServiceImpl.java
@@ -9,7 +9,11 @@ import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    private CustomerRepository customerRepository ;
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll(){
